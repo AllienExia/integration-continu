a84da144e22f9ef513d3d2e9733a2a1ca91a7435
romain janssen <romain.janssen@viacesi.fr>
2017-05-17 09:57:43 +0200

    modif heroku

---
+++ index.js
@@ -0,0 +1,13 @@
+
+var express = require('express');
+var app = express();
+
+app.use('/static', express.static('static'));
+
+app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.listen(80, function () {
+  console.log('Example app listening on port 80!');
+});
