29361620f4ec83bb9d1054a830888c4484b17d3a
romain janssen <romain.janssen@viacesi.fr>
2017-05-17 11:07:33 +0200

    test

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = express();
+var path    = require("path");
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use('/static', express.static(__dirname + '/static'));
 
 app.get('/', function (req, res) {
-   //res.sendFile(path.join(__dirname+'/index.html'));
-    res.send("Hello Worlddesseee");
+   res.sendFile(path.join(__dirname+'/index.html'));
+   //res.send("Hello Worlddesseee");
 });
 
 app.listen(app.get('port'), function() {
