0c5e72bd3669a832d3e8ff721005a0c33cdbfe4d
romain janssen <romain.janssen@viacesi.fr>
2017-05-17 10:20:13 +0200

    heroku fix

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 var express = require('express');
 var app = express();
 
+
+app.set('port', (process.env.PORT || 5000));
+
 app.use('/static', express.static('static'));
 
 app.get('/', function (req, res) {
    res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-app.listen(80, function () {
-  console.log('Example app listening on port 80!');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on porte', app.get('port'));
 });
