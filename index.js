4080262d72b03d886bede7a0b97fcfd28c83ebdd
romain janssen <romain.janssen@viacesi.fr>
2017-05-17 10:31:57 +0200

    change

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-
 var express = require('express');
 var app = express();
 
-
 app.set('port', (process.env.PORT || 5000));
 
-app.use('/static', express.static('static'));
+app.use('/static', express.static(__dirname + '/static'));
 
 app.get('/', function (req, res) {
-   res.sendFile(path.join(__dirname+'/index.html'));
+   //res.sendFile(path.join(__dirname+'/index.html'));
+    res.send("Hello Worlddesseee");
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on porte', app.get('port'));
+  console.log('Node app is running on port', app.get('port'));
 });
